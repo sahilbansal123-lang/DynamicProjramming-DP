48cc17adfe66a60a2281800c45ae40a989ac1b9b
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-03 10:47:16 +0530

    DP

--- src/Tabulation/Fibbonacci_Series.java
+++ src/Tabulation/Fibbonacci_Series.java
@@ -1,4 +1,19 @@
 package Tabulation;
 
 public class Fibbonacci_Series {
+
+    public static int fibbonacciOfN(int n, int[] dp) {
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[] dp = new int[n + 1];
+        System.out.println(fibbonacciOfN(n, dp));
+    }
 }
