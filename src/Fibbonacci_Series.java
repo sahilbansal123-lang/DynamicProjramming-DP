b01dae56d75f53d636c5da6bd3f1b96a938ec2eb
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-03 10:03:29 +0530

    DP

--- src/Fibbonacci_Series.java
+++ src/Fibbonacci_Series.java
@@ -1,2 +1,18 @@
 public class Fibbonacci_Series {
+    public static int fibbonacci(int n, int[] f)  {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (f[n] != 0) {
+            return f[n];
+        }
+        f[n] = fibbonacci(n - 1, f) + fibbonacci(n - 2, f);
+        return f[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[] f = new int[n + 1];
+        System.out.println(fibbonacci(n, f));
+    }
 }
