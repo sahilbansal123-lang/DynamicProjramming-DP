c20f218d87dc838a0bb98ddf345cff7e238250e4
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-05 10:08:19 +0530

    DP

--- src/ClimbingStairWays.java
+++ src/ClimbingStairWays.java
@@ -1,10 +1,18 @@
 public class ClimbingStairWays {
 
-    public static void climbingStairsWays(int n) {
+    public static int climbingStairsWays(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
 
+        return climbingStairsWays(n-1) + climbingStairsWays(n-2);
     }
 
     public static void main(String[] args) {
         int n = 5;
+        System.out.println(climbingStairsWays(n));
     }
 }
