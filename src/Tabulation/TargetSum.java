1e29420a3bc2087f05abb7dd00ea860255216e25
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-06 12:18:01 +0530

    DP

--- src/Tabulation/TargetSum.java
+++ src/Tabulation/TargetSum.java
@@ -2,6 +2,15 @@ package Tabulation;
 
 public class TargetSum {
 
+    public static boolean targetSum(int arr[], int sum) {
+        int n = arr.length;
+        boolean dp[][] = new boolean[n + 1][sum+1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = true;
+        }
+
+    }
+
     public static void main(String[] args) {
 
     }
