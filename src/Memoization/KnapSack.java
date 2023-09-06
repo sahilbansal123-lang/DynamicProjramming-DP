3ef203faec040d80c825ff7b8acfaf221a412079
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-06 10:34:25 +0530

    DP

--- src/Memoization/KnapSack.java
+++ src/Memoization/KnapSack.java
@@ -1,4 +1,37 @@
 package Memoization;
 
 public class KnapSack {
+
+    public static int knapSack(int val[], int wt[], int W, int n, int dp[][]) {
+        if (W == 0 || n == 0) {
+            return 0;
+        }
+
+        if (dp[n][W] != -1) {
+            return dp[n][W];
+        }
+         if (wt[n-1] <= W) {
+             int ans1 = val[n-1] + knapSack(val, wt, W-wt[n-1], n-1, dp);
+             int an2 = knapSack(val, wt, W, n-1, dp);
+
+             dp[n][W] = Math.max(ans1, an2);
+             return dp[n][W];
+         } else {
+             dp[n][W] = knapSack(val, wt, W, n-1, dp);
+             return dp[n][W];
+         }
+    }
+
+    public static void main(String[] args) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+        int dp[][] = new int[val.length + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(knapSack(val, wt, W, val.length, dp));
+    }
 }
