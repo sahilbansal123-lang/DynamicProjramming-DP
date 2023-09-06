3d58397a78c552edb099e7bfbb70780210587f4d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-06 12:34:28 +0530

    DP

--- src/Tabulation/TargetSum.java
+++ src/Tabulation/TargetSum.java
@@ -8,7 +8,17 @@ public class TargetSum {
         for (int i = 0; i < dp.length; i++) {
             dp[i][0] = true;
         }
-
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                int v = arr[i-1];
+                if (v <= j && dp[i-1][j-v]) {
+                    dp[i][j] = true;
+                } else if (dp[i-1][j] == true) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[n][sum];
     }
 
     public static void main(String[] args) {
