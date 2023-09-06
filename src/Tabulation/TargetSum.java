01e32b7b0c41c920f11af452828e65ddfb47dee0
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-06 21:59:13 +0530

    DP

--- src/Tabulation/TargetSum.java
+++ src/Tabulation/TargetSum.java
@@ -22,7 +22,7 @@ public class TargetSum {
 
     public static void main(String[] args) {
         int arr[] = {4, 7, 2, 1, 3};
-        int sum = 10;
+        int sum = 20;
         System.out.println(targetSum(arr, sum));
     }
 }
