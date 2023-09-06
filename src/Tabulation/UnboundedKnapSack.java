4daf823e4caefabba409402c13649d0de4fcf3ff
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-06 22:36:17 +0530

    DP

--- src/Tabulation/UnboundedKnapSack.java
+++ src/Tabulation/UnboundedKnapSack.java
@@ -1,15 +1,15 @@
 package Tabulation;
 
-public class KnapSack {
+public class UnboundedKnapSack {
 
-    public static int knapSack(int[] val, int[] wt, int W) {
+    public static int unBoundedKnapSack(int val[], int wt[], int W) {
         int n = val.length;
-        int[][] dp = new int[n+1][W+1];
+        int dp[][] = new int[n+1][W+1];
 
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < W+1; j++) {
                 if (wt[i-1] <= j) {
-                    dp[i][j] = Math.max(val[i-1] + dp[i-1][j - wt[i-1]], dp[i-1][j]);
+                    dp[i][j] = Math.max(val[i-1] + dp[i][j - wt[i-1]], dp[i-1][j]);
                 } else {
                     dp[i][j] = dp[i-1][j];
                 }
@@ -22,6 +22,6 @@ public class KnapSack {
         int[] val = {15, 14, 10, 45, 30};
         int[] wt = {2, 5, 1, 3, 4};
         int W = 7;
-        System.out.println(knapSack(val, wt, W));
+        System.out.println(unBoundedKnapSack(val, wt, W));
     }
 }
--- src/Tabulation/unBoundedKnapSack.java
+++
@@ -1,9 +0,0 @@
-package Tabulation;
-
-public class unBoundedKnapSack {
-
-
-    public static void main(String[] args) {
-
-    }
-}
