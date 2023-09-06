403d45bd3f218b82d214604e98d923880a4a3770
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-06 12:40:28 +0530

    DP

--- src/Tabulation/TargetSum.java
+++ src/Tabulation/TargetSum.java
@@ -1,7 +1,6 @@
 package Tabulation;
 
 public class TargetSum {
-
     public static boolean targetSum(int[] arr, int sum) {
         int n = arr.length;
         boolean[][] dp = new boolean[n + 1][sum+1];
@@ -9,7 +8,7 @@ public class TargetSum {
             dp[i][0] = true;
         }
         for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n+1; j++) {
+            for (int j = 1; j < sum + 1; j++) {
                 int v = arr[i-1];
                 if (v <= j && dp[i-1][j-v]) {
                     dp[i][j] = true;
