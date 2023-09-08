824fe08f131befcfc035eacd0d319e8044f4479b
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-08 12:12:59 +0530

    DP

--- src/Tabulation/CoinChange.java
+++ src/Tabulation/CoinChange.java
@@ -1,9 +1,9 @@
 package Tabulation;
 
 public class CoinChange {
-    public static int coinChange(int arr[], int sum) {
+    public static int coinChange(int[] arr, int sum) {
         int n = arr.length;
-        int dp[][] = new int[n+1][sum+1];
+        int[][] dp = new int[n+1][sum+1];
 
         for (int i = 0; i < dp.length; i++) {
             dp[i][0] = 1;
@@ -12,7 +12,7 @@ public class CoinChange {
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < sum+1; j++) {
                 if (arr[i-1] <= j) { // valid
-                    dp[i][j] = dp[i][j-arr[i-1]] + dp[i-1][j];;// include
+                    dp[i][j] = dp[i][j-arr[i-1]] + dp[i-1][j];// include
                 } else{
                     dp[i][j] = dp[i-1][j];
                 }
@@ -22,8 +22,8 @@ public class CoinChange {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3};
-        int sum = 4;
+        int[] arr = {2, 3, 5, 6};
+        int sum = 10;
         System.out.println(coinChange(arr, sum));
     }
 }
