57822986dfe438d998718da54c2317c2cba25ca1
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-08 10:50:31 +0530

    DP

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -1,2 +1,15 @@
 public class CoinChange {
+
+    public static int coinChange(int arr[], int sum) {
+
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3};
+        int sum = 4;
+
+        System.out.println(coinChange(arr, sum));
+    }
 }
