d4e0b9cbeb981d485dbfa1c4e45345afcbc7c345
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-11 11:43:38 +0530

    DP

--- src/Recurssion/LongestCommonSubsequence.java
+++ src/Recurssion/LongestCommonSubsequence.java
@@ -2,11 +2,14 @@ package Recurssion;
 
 public class LongestCommonSubsequence {
 
-    public static void lcs(String str1, String str2) {
+    public static void lcs(String str1, int n, String str2, int m) {
 
     }
 
     public static void main(String[] args) {
-
+        String str1 = "abcdeg";
+        String str2 = "abdegf";
+        int n = str1.length();
+        int m = str2.length();
     }
 }
