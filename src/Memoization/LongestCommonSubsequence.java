390be26935256997ab80c887389564dd1cb9e457
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-11 12:33:02 +0530

    DP

--- src/Memoization/LongestCommonSubsequence.java
+++ src/Memoization/LongestCommonSubsequence.java
@@ -2,13 +2,36 @@ package Memoization;
 
 public class LongestCommonSubsequence {
 
-    public static int lcs(String str1, int n, String str2, int m) {
-        int ans = 0;
+    public static int lcs(String str1, int n, String str2, int m, int dp[][]) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
 
-        return ans;
+        if (str1.charAt(n-1) == str2.charAt(m-1)) {
+            dp[n][m] = lcs(str1, n-1, str2, m-1, dp);
+            return dp[n][m] + 1;
+        } else {
+            int ans1 = lcs(str1, n-1, str2, m, dp);
+            int ans2 = lcs(str1, n, str2, m-1, dp);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
     }
 
     public static void main(String[] args) {
+        String str1 = "abcde";
+        String str2 = "ace";
+        int n = str1.length();
+        int m = str2.length();
 
+        int dp[][] = new int[n+1][m+1];
+        for (int i = 0; i < n+1; i++) {
+            for (int j = 0; j < m+1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(lcs(str1, n, str2, m, dp));
     }
 }
