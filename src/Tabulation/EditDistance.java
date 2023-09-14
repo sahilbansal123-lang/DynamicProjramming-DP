c372fbfaeb002fe03d0b4ad2b3845648178dd6e7
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-14 12:15:57 +0530

    DP

--- src/Tabulation/EditDistance.java
+++ src/Tabulation/EditDistance.java
@@ -1,9 +1,45 @@
 package Tabulation;
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 public class EditDistance {
+
+    public static int editDist(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int dp[][] = new int[n+1][m+1];
+
+        for (int i = 0; i < n+1; i++) {
+            for (int j = 0; j < m+1; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                if (j == 0) {
+                    dp[i][j] = i;
+                }
+            }
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    int del = dp[i-1][j] + 1;
+                    int add =  dp[i][j-1] + 1;
+                    int replace = dp[i-1][j-1] + 1;
+                    dp[i][j] = Math.min(add, Math.min(del, replace));
+                }
+            }
+        }
+        return dp[n][m];
+    }
 
 
     public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
 
+        System.out.println(editDist(word1, word2));
     }
 }
