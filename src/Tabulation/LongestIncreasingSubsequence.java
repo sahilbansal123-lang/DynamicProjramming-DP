3df0caf2918f9a34f6c86a2246aca9ea19814d56
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-14 10:24:51 +0530

    DP

--- src/Tabulation/LongestIncreasingSubsequence.java
+++ src/Tabulation/LongestIncreasingSubsequence.java
@@ -5,12 +5,21 @@ import java.util.HashSet;
 public class LongestIncreasingSubsequence {
 
     public static int LIS(int arr[]) {
-        HashSet<Integer> set = new HashSet<Integer>();
+
+//      ------------------SORTING OF ARRAY INTO ARR2-----------------------
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             set.add(arr[i]);
         }
-        int arr2[] = new int[set.size()];
-        arr2 = set.toArray(arr2);
+        int[] arr2 = new int[set.size()];
+        int i = 0;
+        for (int num: set) {
+            arr2[i] = num;
+            i++;
+        }
+//      --------------------------------------------------------------------
+
+
     }
 
     public static void main(String[] args) {
