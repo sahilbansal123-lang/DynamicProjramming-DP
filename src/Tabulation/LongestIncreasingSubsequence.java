ac8fe464a012f783937887ca2a045356fdd138df
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-14 10:20:50 +0530

    DP

--- src/Tabulation/LongestIncreasingSubsequence.java
+++ src/Tabulation/LongestIncreasingSubsequence.java
@@ -1,6 +1,17 @@
 package Tabulation;
 
+import java.util.HashSet;
+
 public class LongestIncreasingSubsequence {
+
+    public static int LIS(int arr[]) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        int arr2[] = new int[set.size()];
+        arr2 = set.toArray(arr2);
+    }
 
     public static void main(String[] args) {
 
