11b877b7c28f8243ad7a8c62f3827840443464ee
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-15 12:34:16 +0530

    DP

--- src/Memoization/CatlansNumber.java
+++ src/Memoization/CatlansNumber.java
@@ -1,9 +1,30 @@
 package Memoization;
 
+import java.util.Arrays;
+
 public class CatlansNumber {
 
+    public static int findCatlan(int n, int dp[]) {
+        if (n == 0 || n == 1) {
+            dp[n] = 1;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += findCatlan(i, dp) * findCatlan(n - i - 1, dp);
+
+        }
+        return dp[n] = ans;
+    }
 
     public static void main(String[] args) {
-
+        int n = 4;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(findCatlan(n, dp));
     }
 }
--- src/Tabulation/CatlansNumber.java
+++ src/Tabulation/CatlansNumber.java
@@ -1,4 +1,8 @@
 package Tabulation;
 
 public class CatlansNumber {
+
+    public static void main(String[] args) {
+
+    }
 }
