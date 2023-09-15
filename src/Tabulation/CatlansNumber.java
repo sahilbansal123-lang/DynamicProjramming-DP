b5789b1d354654efa98f170872374a8ad671f50a
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-15 12:39:36 +0530

    DP

--- src/Tabulation/CatlansNumber.java
+++ src/Tabulation/CatlansNumber.java
@@ -2,7 +2,21 @@ package Tabulation;
 
 public class CatlansNumber {
 
-    public static void main(String[] args) {
+    public static int catlans(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
 
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += (dp[j] * dp[i - j - 1]);
+            }
+        }
+        return dp[n];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(catlans(4));
     }
 }
