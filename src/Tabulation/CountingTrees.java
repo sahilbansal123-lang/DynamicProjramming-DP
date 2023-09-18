14daf7e5218a134136dc3bdb63d440337ae81631
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-18 11:34:10 +0530

    DP

--- src/Tabulation/CountingTrees.java
+++ src/Tabulation/CountingTrees.java
@@ -3,12 +3,24 @@ package Tabulation;
 public class CountingTrees {
 
     public static int noOfTrees(int n, int arr[]) {
-        int count = 0;
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        return count;
+        for (int i = 2; i < n+1; i++) {
+            for (int j = 0; j < i; j++) {
+                int left = dp[j];
+                int right = dp[i-j-1];
+                dp[i] += left * right;
+            }
+        }
+        return dp[n];
     }
 
     public static void main(String[] args) {
+        int n = 4;
+        int arr[] = {10, 20, 30, 40};
 
+        System.out.println(noOfTrees(n, arr));
     }
 }
