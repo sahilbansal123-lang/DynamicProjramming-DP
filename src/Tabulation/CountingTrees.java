87f06dcef9617c3a0f77310c02227d2ea6704914
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-18 11:11:04 +0530

    DP

--- src/Tabulation/CountingTrees.java
+++ src/Tabulation/CountingTrees.java
@@ -2,8 +2,10 @@ package Tabulation;
 
 public class CountingTrees {
 
-    public static int noOfTrees() {
+    public static int noOfTrees(int n, int arr[]) {
+        int count = 0;
 
+        return count;
     }
 
     public static void main(String[] args) {
