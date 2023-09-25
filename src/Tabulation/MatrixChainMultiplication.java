d4d16cbc6af6e1b4b0e3ade99df7966dae429d49
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-25 10:23:05 +0530

    DP

--- src/Tabulation/MatrixChainMultiplication.java
+++ src/Tabulation/MatrixChainMultiplication.java
@@ -3,7 +3,16 @@ package Tabulation;
 public class MatrixChainMultiplication {
 
     public static int mcm(int arr[], int i, int j) {
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k < j-1; k++) {
+            int cost1 = mcm(arr, i, k);
+            int cost2 = mcm(arr, k+1, j);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
 
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
