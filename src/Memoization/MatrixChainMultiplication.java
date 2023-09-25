4ad0b1ff688dfc6c93d3130b3a1d38654d433afd
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-25 10:53:34 +0530

    DP

--- src/Memoization/MatrixChainMultiplication.java
+++ src/Memoization/MatrixChainMultiplication.java
@@ -1,9 +1,18 @@
 package Memoization;
 
+import java.util.Arrays;
+
 public class MatrixChainMultiplication {
 
+    public stati
 
     public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 3};
+        int n = arr.length;
 
+        int dp[][] = new int[n][n];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
     }
 }
