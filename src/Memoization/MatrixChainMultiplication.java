18be556cd0afbe7eddbc39eefdc3b413d53a7bc0
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-25 10:58:42 +0530

    DP

--- src/Memoization/MatrixChainMultiplication.java
+++ src/Memoization/MatrixChainMultiplication.java
@@ -4,7 +4,25 @@ import java.util.Arrays;
 
 public class MatrixChainMultiplication {
 
-    public stati
+    public static int mcm(int arr[], int i, int j, int dp[][]) {
+        if (i == j) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int minCost = Integer.MAX_VALUE;
+        for (int k = i; k < j-1; k++) {
+
+            int cost1 = mcm(arr, i, k, dp);
+            int cost2 = mcm(arr, k+1, j, dp);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+
+            minCost = Math.min(minCost, finalCost);
+        }
+        return dp[i][j] = minCost;
+    }
 
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 3};
