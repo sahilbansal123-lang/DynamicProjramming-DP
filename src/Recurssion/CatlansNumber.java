1e2922bd205c43875b5d57c807db28e7e68a7e4e
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-28 10:32:14 +0530

    DP

--- src/Recurssion/CatlansNumber.java
+++ src/Recurssion/CatlansNumber.java
@@ -2,11 +2,9 @@ package Recurssion;
 
 public class CatlansNumber {
     public static int findCatlan(int n) {
-
         if (n == 0 || n == 1) {
             return 1;
         }
-
         int ans = 0;
         for (int i = 0; i < n; i++) {
             ans += (findCatlan(n-i-1) * findCatlan(i));
