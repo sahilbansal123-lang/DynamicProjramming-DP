d2930bf0a579827d193a56d3588853a014a1ba9b
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-29 11:09:45 +0530

    DP

--- src/Tabulation/MinArrayJumps.java
+++ src/Tabulation/MinArrayJumps.java
@@ -2,8 +2,32 @@ package Tabulation;
 
 public class MinArrayJumps {
 
+    public static int minJumps(int nums[]) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        dp[n-1] = 0;
+        for (int i = 0; i < n-1; i++) {
+            dp[i] = -1;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = n-2; i >= 0; i++) {
+            int steps = nums[i];
+            for (int j = i+1; j <= i + steps && j < n; j++) {
+                if (dp[j] != -1) {
+                    ans = Math.min(ans, dp[j] + 1);
+                }
+            }
+            if (dp[i] != Integer.MAX_VALUE) {
+                dp[i] = ans;
+            }
+        }
+        return dp[0];
+    }
+
 
     public static void main(String[] args) {
-
+        int nums[] = {2, 3, 1, 1, 4};
+        System.out.println(minJumps(nums));
     }
 }
